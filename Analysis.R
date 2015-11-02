b4a4fcf1862b3ffcfb335f8ce915f932be0abc7c
Oskar Harmsen <oskar.harmsen@gmail.com>
2015-11-02 12:53:48 +0100

    Updates to loading data

--- Analysis.R
+++ Analysis.R
@@ -10,3 +10,16 @@ library("plyr")
 ### Load data
   df <- read.csv("Assign2.csv", header = TRUE, sep = ";")
   names(df) <- c("date", "type", "town", "district", "views", "department", "currency", "paid", "title")
+  
+  #Correct classes of columns
+  df$title <- str_trim(df$title)
+  df$date <- as.Date(df$date)
+  df$views <- as.numeric(df$views)
+  df$paid <- as.numeric(df$paid)
+  
+  #Remove NAs and Currency
+  df <- df %>% 
+        select(-c(currency)) %>% 
+        filter(!is.na(paid)) %>% 
+  
+  
