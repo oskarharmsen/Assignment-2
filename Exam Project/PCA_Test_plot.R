e0214aec7496e9b532e3b4807d6e06c5a7ff049e
Oskar Harmsen <oskar.harmsen@gmail.com>
2015-11-18 18:38:18 +0100

    Plot of average response by party

--- Exam Project/PCA_Test_plot.R
+++ Exam Project/PCA_Test_plot.R
@@ -29,12 +29,7 @@ data <- data.org
   
   data[20:24] <- pc$scores[,1:5]
   
-  p <- ggplot(data = data, aes(x = data[,20], y = data[,21] )) +
-        geom_point(aes(color=party), size=10, alpha=0.8)+
-        scale_colour_manual(values = c("black", "red", "darkolivegreen4", "darkorchid4", "lightgreen", "hotpink", "cyan1", "grey", "yellow", "orange", "darkblue"))+
-        theme_minimal()
-  p
-  
+
   #Pretty Plot#
   data2 = filter(data, party!=1)
   
@@ -60,3 +55,88 @@ data <- data.org
 
   View(data)
   
+  #Variance in responses
+  
+  resp.var <- data[,5:19] %>% 
+              var() %>% 
+              diag() %>% 
+              sqrt() %>% 
+              t()
+            
+  rownames(resp.var) <- "Standard Deviation"
+
+  
+  ## -- Add mean response for each party, for each question -- ##
+  
+  party.means <- data %>% 
+            filter(party != 1) %>% 
+            group_by(party) %>%
+            summarize_each(funs(mean), -c(name, age, votes))
+
+  ## --- Plot average response to each question, by party --- #
+  
+  # Construct labels
+    labels <- data.frame(
+      question = names(party.means[2:16]),
+      position.y = 16:2+0.5,
+      position.x = rep(3, 15)
+    )
+  
+  
+  # Build plot
+  p <-  ggplot(data = party.means)
+        
+        #Loop over each question, and plot the party means
+        for(i in 2:16){
+          p <- p + 
+            #Plot party means
+            geom_point(aes_string(
+            y = 18-i, 
+            x = paste("party.means$", names(party.means)[i], sep = ""),
+            fill = "party"
+            ),
+            colour = "black", alpha=0.8, shape = 21, size = 10
+            )
+        }
+  
+        #Format plot
+        p <- p +
+        
+        #Add questions  
+        geom_text(data = labels,
+                  aes( y = position.y, x = position.x, label = question),
+                  size = 3) +
+        
+        #Party colors  
+        scale_fill_manual(values = c("red",             # Socialdemokratiet
+                                     "darkolivegreen4", # Alternativet
+                                     "darkorchid4",     # Radikale
+                                     "lightgreen",      # Konservative
+                                     "hotpink",         # SF
+                                     "cyan1",           # Liberal Alliance
+                                     "grey",            # Kristendemokraterne
+                                     "yellow",          # Dansk Folkeparti
+                                     "orange",          # Enhedslisten 
+                                     "darkblue"),     # Venstre    
+                          labels = c("S",             # Socialdemokratiet
+                                     "Å", # Alternativet
+                                     "R",     # Radikale
+                                     "C",      # Konservative
+                                     "SF",         # SF
+                                     "Y",           # Liberal Alliance
+                                     "K",            # Kristendemokraterne
+                                     "O",          # Dansk Folkeparti
+                                     "Ø",          # Enhedslisten 
+                                     "V")) +     # Venstre    
+        theme_minimal() +            
+        theme(axis.title.y  = element_blank(),
+              axis.text.y = element_blank(),
+              axis.title.x = element_blank(),
+              panel.grid.minor=element_blank(),
+              legend.position="top") +
+        scale_y_continuous(breaks=seq(1, 16, 1)) +
+        scale_x_continuous(breaks=c(1,3,5),
+                           labels=c("Highly disagree", "Neither agree nor disagree", "Highly agree"))+
+        ggtitle("Mean response to survey \n questions, by party")
+        
+  p      
