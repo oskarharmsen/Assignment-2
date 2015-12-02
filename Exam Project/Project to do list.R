394ae79f5e62cae93c8c15c34cf99738850d6104
Oskar Harmsen <oskar.harmsen@gmail.com>
2015-12-02 23:00:54 +0100

    Small changes.

--- Exam Project/Project to do list.R
+++ Exam Project/Project to do list.R
@@ -17,11 +17,16 @@
   #     (party averages, kreds-averages, location averages, east-west coordinate)
   #
   #
-  #
   # Are parties distinct? (The political landscape)
   #   - Build decision tree or other prediction model on survey responses, check errors by party
   #   
   #
   #
-  #
+  # IDEA
   # 
+  # Kan man udtrække breddegrad fra Barforts pakke DKMap? Så burde man kunne se hvor langt mod vest
+  # hver valgkreds ligger, og kunne lave plots inden for hvert parti for at se om der er en øst-vest
+  # faktor i dansk politik 
+  # 
+  # 
+  
---
+++ Exam Project/Short version, script.R
@@ -0,0 +1,18 @@
+#### Do a short version ####
+
+setwd("/Users/oskarh/Documents/Assignment 2/Group 8/Exam Project")
+
+
+data1 <- read.csv(file = "dk_ft15_politician_responses.csv", header = TRUE) #Load the raw dataset
+data2 <- read.csv(file = "final.csv", header = TRUE) #Load the raw dataset
+
+names(data2)[4:18] <- names(data1)[5:19]
+
+data2 <-  data2 %>% 
+          select( -X) %>% 
+          arrange(Alder) %>%
+          rename(pers_stemmer = Stemmer)
+          
+data.head <- data2[1:10,]
+
+write.table(x = data.head, file = "head_dk_ft15_politician_responses.csv", fileEncoding = "UTF-8", sep = ",", row.names = FALSE)
